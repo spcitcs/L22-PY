f91d2129afd6fe0ff32b8ab928f338f6466a6423
alfianisnan26 <fianshared@gmail.com>
2022-09-02 22:44:33 +0700

    [alfianisnan26] adding runner

---
+++ runner.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import os
+import sys
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    n = 0
+    print(f"[ RUNNING - {user} ]")
+    for module in os.listdir("."):
+        taskDir = os.path.join(module, "tasks", user)
+        if(os.path.isdir(module) and os.path.isdir(taskDir)):
+            for task in os.listdir(taskDir):
+                dir = os.path.join(taskDir, task)
+                with open(dir) as f:
+                    firstline = f.readlines()[0].rstrip()
+                if "--RUN" in firstline:
+                    n += 1
+                    print(f"\n[ TASK - {n} ]")
+                    print(f"[ Running from {dir} ]\n")
+                    start = datetime.now()
+                    exec(open(dir).read())
+                    end = datetime.now()
+                    print(f"\n[ Exec Time : {end-start} ]")
+    if(n == 0):
+        print("[ ERROR ]")
+        print("[ No Task with tag --RUN found! ]")
+        print("[ Please add --RUN tag to your files to run! ]")
+    print("[ FINISHED ]")
