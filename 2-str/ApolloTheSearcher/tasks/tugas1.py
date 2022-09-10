69290151575e6435064eda230d2d3a35087a1c0b
ApolloTheSearcher <gentaputra378@gmail.com>
2022-09-10 12:22:59 +0700

    [Gentha Ardaana] TUGAS 1

--- 2-str/ApolloTheSearcher/tasks/tugas1.py
+++ 2-str/ApolloTheSearcher/tasks/tugas1.py
@@ -2,7 +2,7 @@
 # Aplikasi sederhana biodata
 
 # Selamat datang di dasbor...
-#
+
 # ------------------------------ Data Diri [1] ----------------------------------
 # Nama              : Nama Kalian
 # Nama Depan        :
@@ -20,7 +20,7 @@
 # LinkedIn          : (Yang ada linknya)
 
 # Selamat datang di dasbor...
-#
+
 
 # ------------------------------ Data Diri [2] ----------------------------------
 # Nama              : Nama Kalian
@@ -39,7 +39,7 @@
 # LinkedIn          : (Yang ada linknya)
 
 # Selamat datang di dasbor...
-#
+
 # ------------------------------ Data Diri [3] ----------------------------------
 # Nama              : Nama Kalian
 # Nama Depan        :
@@ -57,3 +57,17 @@
 # LinkedIn          : (Yang ada linknya)
 
 # Memakai formatting dan menggunakan variable nya
+nameList = ["Gentha", "Jokowi", "Prabowo"]
+lastName = ["Ardaana", "Widodo", "Subianto"]
+kelas = ["12 IPA 3", "12 IPS 2", "12 IPS 1"]
+for name in range(len(nameList)):
+    welcome = "Selamat datang di dasbor..."
+    header = f"""---------------------------------- Data Diri [{name + 1}]---------------------------------
+    Nama            : {nameList[name] +" "+ lastName[name]}
+    Nama Depan      : {nameList[name]} 
+    Nama Belakang   : {lastName[name]}
+---------------------------------- Data Sekolah ------------------------------------------------
+    Nama Sekolah    : SMAN 1 CIBADAK
+    Kelas           : {kelas[name]}
+    """
+    print(header)
